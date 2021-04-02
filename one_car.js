c6d7cf780e6f0f221156cd94751bf8cf564a2b80
Nicolaj Baramsky <nicolaj.baramsky@gmail.com>
2021-04-02 17:24:07 +0200

    Update one_car.js

--- one_car.js
+++ one_car.js
@@ -19,9 +19,9 @@ Script.complete()
     date_diff_ms = current_date.getTime() - start_date.getTime()
 
     date_diff_days = date_diff_ms/ 1000 /3600 /24
-    date_diff_years = ceil(date_diff_days/365)
+    date_diff_years = date_diff_days/365
 
-    km_total = float(km/365*date_diff_days).toFixed(3)
+    km_total = km/365*date_diff_days
   }
 
 function createWidget() {
@@ -35,13 +35,13 @@ function createWidget() {
   
   w.addSpacer(5)
   
-  let titleTxt = w.addText("100.000")
+  let titleTxt = w.addText(km_total)
   titleTxt.textColor = Color.black()
   titleTxt.font = Font.systemFont(22)
   
   w.addSpacer(5)
-  
-  let subTxt = w.addText("Unter-Überschrift")
+  km_uebrig = int(km*date_diff_years-km_total) + " (" + int((km*date_diff_years-km_total)/km*100) + "%)"
+  let subTxt = w.addText(km_uebrig)
   subTxt.textColor = Color.black()
   subTxt.textOpacity = 0.8
   subTxt.font = Font.systemFont(18)
